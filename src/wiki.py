f79e5e434159e47e5d7e7e22d87f89d536df4883
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-02-27 15:02:37 +0100

    cache wiki requests

--- src/wiki.py
+++ src/wiki.py
@@ -1,14 +1,18 @@
 import wikipedia
+from functools import lru_cache
+
 
 wikipedia.set_lang("en")
 
 
 class WikiParser:
 
+    @lru_cache(maxsize=1000)
     def get_articles(self, long, lat):
         articles = wikipedia.geosearch(long, lat, results=10, radius=10_000)
         return [wikipedia.page(article).content for article in articles]
 
+    @lru_cache(maxsize=1000)
     def get_summaries(self, long, lat):
         articles = wikipedia.geosearch(long, lat, results=10, radius=10_000)
         return [wikipedia.page(article).summary for article in articles]
