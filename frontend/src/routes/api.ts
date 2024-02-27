61e9a8f4e13ec2113d4f52e9cc558d69b4789f96
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2024-02-27 16:03:49 +0100

    add places string option to location api

--- frontend/src/routes/api.ts
+++ frontend/src/routes/api.ts
@@ -1,34 +1,39 @@
-
 import {type location } from "./lcoation_search";
 
-export async function getLocation(start:location, end:location, distance: number): Promise<{ long: number, lat: number }> {
-    const response = await fetch(`/location?start_long=${start.lon}&start_lat=${start.lat}&destination_long=${end.lon}&destination_lat=${end.lat}&distance=${distance}`, {
+const backend_url = 'http://localhost:5000';
+// const backend_url = 'http://10.52.249.109:5000';
+
+
+export async function getLocation(start:location|string, end:location|string, distance: number): Promise<{ long: number, lat: number }> {
+    const startparam = typeof start === 'string' ? `start=${start}` : `start_long=${start.lon}&start_lat=${start.lat}`;
+    const endparam = typeof end === 'string' ? `destination=${end}` : `destination_long=${end.lon}&destination_lat=${end.lat}`;
+    const response = await fetch(`${backend_url}/location?${startparam}&${endparam}&distance=${distance}`, {
         method: 'GET',
     });
     if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok:' + response.status);
     }
     return await response.json();
 }
 
 
 export async function getFunFacts(loc:location): Promise<{ text: string }> {
-    const response = await fetch(`/funfacts?long=${loc.lon}&lat=${loc.lat}`, {
+    const response = await fetch(`${backend_url}/funfacts?long=${loc.lon}&lat=${loc.lat}`, {
         method: 'GET',
     });
     if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok:' + response.status);
     }
     return await response.json();
 }
 
 
 export async function getAudioGuide(long:location): Promise<Blob> {
-    const response = await fetch(`/audioguide?long=${long.lon}&lat=${long.lat}`, {
+    const response = await fetch(`${backend_url}/audioguide?long=${long.lon}&lat=${long.lat}`, {
         method: 'GET',
     });
     if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok:' + response.status);
     }
     return await response.blob();
 }
