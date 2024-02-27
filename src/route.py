48f51337a089b2acdb9566af49b8195d5a171667
Minh An Pham <minhanp@gmail.com>
2024-02-27 14:36:01 +0100

    finished up routes

--- src/route.py
+++ src/route.py
@@ -1,14 +1,20 @@
 import requests
-import json
 import polyline
 from geopy.distance import geodesic
 
-import requests
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+dotenv_path = Path("../.env")
+load_dotenv(dotenv_path=dotenv_path)
+google_api_key = os.getenv("GOOGLE")
 
 
 def get_coordinates(location):
+    # distance in km
     # Construct the request URL
-    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={api_key}'
+    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={google_api_key}'
 
     # Send the request
     response = requests.get(url)
@@ -25,22 +31,15 @@ def get_coordinates(location):
         print(f"Error: {data['status']} - {data.get('error_message', '')}")
         return None, None
 
-# Replace 'YOUR_API_KEY' with your actual API key
-api_key = 'YOUR_API_KEY'
-location = 'New York'
-
-latitude, longitude = get_coordinates(location, api_key)
-if latitude is not None and longitude is not None:
-    print(f"Coordinates of {location}: Latitude {latitude}, Longitude {longitude}")
-
-
 class route():
     def __init__(self, start, dest):
 
-        if type(start) == "str":
-            get_coordinates(start)
-        if type(start) == "str":
-            get_coordinates(dest)
+        #check for being string
+        if type(start) == str:
+            start = get_coordinates(start)
+        if type(dest) == str:
+            dest = get_coordinates(dest)
+
         self.start = start
         self.dest = dest
         # Define the request payload
@@ -48,16 +47,16 @@ class route():
         "origin": {
             "location": {
             "latLng": {
-                "latitude": start[0],
-                "longitude": start[1]
+                "latitude": f'{start[0]}',
+                "longitude": f'{start[1]}'
             }
             }
         },
         "destination": {
             "location": {
             "latLng": {
-                "latitude": dest[0],
-                "longitude": dest[1]
+                "latitude": f'{dest[0]}',
+                "longitude": f'{dest[1]}'
             }
             }
         },
@@ -76,15 +75,16 @@ class route():
         # Define the headers
         headers = {
             'Content-Type': 'application/json',
-            'X-Goog-Api-Key': 'AIzaSyCLTR5jt4ev0PdxstLdqYjuFUxeGnzVq7M',
+            'X-Goog-Api-Key': google_api_key,
             'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
         }
 
         # Make the request
         self.response_json = requests.post('https://routes.googleapis.com/directions/v2:computeRoutes', json=payload, headers=headers).json()
-
-        self.encoded_polyline = self.response_json['routes'][0]['overview_polyline']['points']
+        print(self.response_json)
+        self.encoded_polyline = self.response_json['routes'][0]['polyline']['encodedPolyline']
         self.decoded_polyline = polyline.decode(self.encoded_polyline)
+
     def get_point(self, distance):
 
         # Initialize variables
