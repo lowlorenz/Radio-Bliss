9014ce6c93d71de923137189f0bc07a909c7488d
dkormann <dominikormann@outlook.com>
2024-02-27 12:45:41 +0100

    move

--- examples/llmchat.py
+++ examples/llmchat.py
@@ -1,10 +1,12 @@
 #%%
-import dotenv
 from openai import OpenAI
+import dotenv
 
 dotenv.load_dotenv()
-
 key = dotenv.get_key("./.env", "OPENAI")
+
+
+
 client = OpenAI(api_key=key,base_url="https://llms.azurewebsites.net")
 
 
@@ -16,3 +18,10 @@ stream = client.chat.completions.create(
 for chunk in stream:
     if chunk.choices[0].delta.content is not None:
         print(chunk.choices[0].delta.content, end="")
+
+
+
+
+# %%
+
+# %%
