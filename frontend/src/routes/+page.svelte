291934f3a85f329eb91c04a60a05e7a0f728b199
dkormann <dominikormann@outlook.com>
2024-02-28 11:47:39 +0100

    title ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,9 +5,14 @@
 </script>
 
 
-<h1>Welcome to Radio-Bliss</h1>
+<h1> 
+    <img id=blogo src="./logo_bliss_white.svg" alt="">
+    Welcome to Radio-Bliss
+</h1>
 
 
 <RoutePlanner />
 <!-- <div id=bgimage></div> -->
+
+
 
